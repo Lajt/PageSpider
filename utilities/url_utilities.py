a2aa1b3da54a773ebcfd3798f6f716627e3f72ef
Lajt <matti815@gmail.com>
2018-04-17 00:16:52 +0200

    fixed formatting issues

--- utilities/url_utilities.py
+++ utilities/url_utilities.py
@@ -1,11 +1,13 @@
 def load_urls_from_file(file_path: str):
-    #TODO: read text file
+    # TODO: read text file
     pass
+
 
 def load_page(url: str):
-    #TODO: read website
+    # TODO: read website
     pass
 
+
 def scrape_page(page_contents: str):
-    #TODO: anlyze text
+    # TODO: anlyze text
     pass
