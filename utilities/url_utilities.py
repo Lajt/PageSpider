628cfc8925bd370936ad6da14f4e22513adbd072
Lajt <matti815@gmail.com>
2018-04-16 23:43:37 +0200

    initial commit

---
+++ utilities/url_utilities.py
@@ -0,0 +1,11 @@
+def load_urls_from_file(file_path: str):
+    #TODO: read text file
+    pass
+
+def load_page(url: str):
+    #TODO: read website
+    pass
+
+def scrape_page(page_contents: str):
+    #TODO: anlyze text
+    pass
